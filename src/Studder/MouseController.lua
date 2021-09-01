cc24ba48bf4f1a30842baa137fe06517f34bb5ca
u-train <skassem05@outlook.com>
2021-09-01 13:16:26 -0400

    Get rid of service never used.

--- src/Studder/MouseController.lua
+++ src/Studder/MouseController.lua
@@ -1,4 +1,3 @@
-local ServerScriptService = game:GetService("ServerScriptService")
 local UserInputService = game:GetService("UserInputService")
 local Roact = require(script.Parent.Parent.Libraries.Roact)
 

6264b41864e586749e56f94065f5e46600aa9d02
u-train <skassem05@outlook.com>
2022-01-04 16:40:24 -0500

    Add an init module for common components.

---
+++ src/Common/init.lua
@@ -0,0 +1,7 @@
+return {
+	ColorInput = require(script.ColorInput),
+	ControlledInput = require(script.ControlledInput),
+	LabelledInput = require(script.LabelledInput),
+	ListLayout = require(script.ListLayout),
+	Topbar = require(script.Topbar)
+}

8fcb206f59aad5be1206de3e5a98d1ed679ba171
u-train <skassem05@outlook.com>
2022-01-12 18:45:01 -0500

    Prepare ColorInput.lua added scaffolding for the component

--- src/Common/ColorInput.lua
+++ src/Common/ColorInput.lua
@@ -1,1 +1,11 @@
-local Roact = require(script.Parent.Parent.Libraries.Roact)
+local Roact = require(script.Parent.Parent.Libraries.Roact) :: Roact
+
+local ColorInputComponent = Roact.Component:extend("ColorInput")
+
+function ColorInputComponent:init()
+end
+
+function ColorInputComponent:render()
+end
+
+return ColorInputComponent

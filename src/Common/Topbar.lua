52497289ec6b9639fd99ccd6327c712052c2339b
u-train <skassem05@outlook.com>
2022-01-18 10:09:21 -0500

    Retrofitted Topbar with studio component

--- src/Common/Topbar.lua
+++ src/Common/Topbar.lua
@@ -1,5 +1,7 @@
-local Roact = require(script.Parent.Parent.Packages.Roact)
-local RoactRouter = require(script.Parent.Parent.Packages.RoactRouter)
+local Packages = script.Parent.Parent.Packages
+local Roact = require(Packages.Roact)
+local RoactRouter = require(Packages.RoactRouter)
+local StudioComponents = require(Packages.StudioComponents)
 
 return function(Props)
 	return RoactRouter.withRouter(function(RouterInfo)
@@ -8,21 +10,21 @@ return function(Props)
 				Position = Props.Position,
 				Size = Props.Size,
 			}, {
-				ReturnBack = Roact.createElement("TextButton", {
+				ReturnBack = Roact.createElement(StudioComponents.Button, {
 					Size = UDim2.new(0, 25, 1, 0),
 					Text = "<",
-					[Roact.Event.MouseButton1Click] = function()
+					OnActivated = function()
 						RouterInfo.history:goBack()
 					end,
 				}),
-				Title = Roact.createElement("TextLabel", {
+				Title = Roact.createElement(StudioComponents.Label, {
 					Size = UDim2.new(1, -25, 1, 0),
 					Position = UDim2.new(0, 25, 0, 0),
 					Text = Props.Title,
 				}),
 			})
 		else
-			return Roact.createElement("TextLabel", {
+			return Roact.createElement(StudioComponents.Label, {
 				Position = Props.Position,
 				Size = Props.Size,
 				Text = Props.Title,

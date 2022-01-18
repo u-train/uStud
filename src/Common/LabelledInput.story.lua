88d073f2bc71d3665f54f0d35b05564a154829fc
u-train <skassem05@outlook.com>
2022-01-18 16:02:52 -0500

    Added stroy for LabelledInput

--- src/Common/LabelledInput.story.lua
+++ src/Common/LabelledInput.story.lua
@@ -1,11 +1,41 @@
-return function(Parent)
-	local Message = Instance.new("TextLabel")
-	Message.Size = UDim2.new(1, 0, 1, 0)
-	Message.Text = "This component's story hasn't been implemented yet."
+local Roact = require(script.Parent.Parent.Packages.Roact) :: Roact
+local LabelledInput = require(script.Parent.LabelledInput)
 
-	Message.Parent = Parent
+local Helper = Roact.Component:extend("Helper")
+
+function Helper:init()
+	self:setState({
+		Value = "",
+	})
+end
+
+function Helper:render()
+	return Roact.createFragment({
+		Input = Roact.createElement(LabelledInput, {
+			Label = "Label",
+			Value = self.state.Value,
+			Size = UDim2.new(1, 0, 0, 25),
+			OnValueChanged = function(Value)
+				self:setState({
+					Value = Value
+				})
+			end,
+		}),
+		Text = Roact.createElement("TextLabel", {
+			Text = self.state.Value,
+			Position = UDim2.new(0, 0, 0, 25),
+			Size = UDim2.new(1, 0, 0, 25),
+		})
+	})
+end
+
+return function(Parent)
+	local Handle = Roact.mount(
+		Roact.createElement(Helper, {}),
+		Parent
+	)
 
 	return function()
-		Message:Destroy()
+		Roact.unmount(Handle)
 	end
 end

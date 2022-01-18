939f473c18b240fd2d40f0bbabc4a93eea7523f1
u-train <skassem05@outlook.com>
2022-01-18 10:16:02 -0500

    Get rid of init for common

--- src/Common/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-	ColorInput = require(script.TextColorInput),
-	ControlledInput = require(script.ControlledInput),
-	LabelledInput = require(script.LabelledInput),
-	Topbar = require(script.Topbar),
-}

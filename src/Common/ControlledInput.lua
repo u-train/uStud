0434f9d290d6e5ec4823c9901e5b050309a9e769
u-train <skassem05@outlook.com>
2022-01-18 10:26:50 -0500

    Patched ControlledInput to use studiocomp

--- src/Common/ControlledInput.lua
+++ src/Common/ControlledInput.lua
@@ -1,4 +1,6 @@
-local Roact = require(script.Parent.Parent.Packages.Roact)
+local Packages = script.Parent.Parent.Packages
+local Roact = require(Packages.Roact)
+local StudioComponents =require(Packages.StudioComponents)
 
 local ControlledInput = Roact.Component:extend("ControlledInput")
 
@@ -8,11 +10,11 @@ end
 
 function ControlledInput:render()
 	self.UpdateInternalValue(self.props.Value)
-	return Roact.createElement("TextBox", {
-		Text = self.InternalValue,
+	return Roact.createElement(StudioComponents.TextInput, {
+		Text = self.InternalValue:getValue(),
 		Size = self.props.Size or UDim2.new(1, 0, 0, 25),
 		Position = self.props.Position,
-		LayoutOrder = self.props.LayoutOrder or 0,
+		LayoutOrder = self.props.LayoutOrder,
 		BorderSizePixel = self.props.BorderSizePixel or 0,
 
 		TextColor3 = self.props.TextColor3,
@@ -20,17 +22,17 @@ function ControlledInput:render()
 		BorderColor3 = self.props.BorderColor3,
 		BorderMode = Enum.BorderMode.Inset,
 
-		[Roact.Change.Text] = function(Rbx)
-			self.UpdateInternalValue(Rbx.Text)
+		OnChanged = function(Text)
+			self.UpdateInternalValue(Text)
 		end,
-		[Roact.Event.Focused] = function()
+		OnFocus = function()
 			if self.props.ClearTextOnFocus then
 				self.UpdateInternalValue("")
 			end
 		end,
-		[Roact.Event.FocusLost] = function(Rbx)
-			self.props.OnValueChanged(Rbx.Text)
-			Rbx.Text = self.props.Value
+		OnFocusLost = function(_)
+			self.props.OnValueChanged(self.InternalValue:getValue())
+			-- Rbx.Text = self.props.Value
 		end,
 
 		ClearTextOnFocus = false,

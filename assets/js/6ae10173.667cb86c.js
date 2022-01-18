e0c46fad43a94242a88af614f9612fd7865049a3
u-train <skassem05@outlook.com>
2022-01-18 18:55:44 -0500

    init?

---
+++ assets/js/6ae10173.667cb86c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[794],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/u-train/uStud/blob/master","baseUrl":"/uStud/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/7aca5e3d.8b8fbb7f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[814],{67243:function(e){e.exports=JSON.parse('[{"value":"Types","id":"types","children":[{"value":"Props","id":"Props","children":[],"level":3}],"level":2},{"value":"Properties","id":"properties","children":[],"level":2},{"value":"Functions","id":"functions","children":[],"level":2}]')}}]);

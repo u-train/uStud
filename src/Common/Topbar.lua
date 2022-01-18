cadbbf0d8fbd8bac60707a869d374fb1aaa87461
u-train <skassem05@outlook.com>
2022-01-18 15:33:25 -0500

    Topbar storied

--- src/Common/Topbar.lua
+++ src/Common/Topbar.lua
@@ -1,37 +1,36 @@
 local Packages = script.Parent.Parent.Packages
 local Roact = require(Packages.Roact)
-local RoactRouter = require(Packages.RoactRouter)
 local StudioComponents = require(Packages.StudioComponents)
 
 return function(Props)
-	return RoactRouter.withRouter(function(RouterInfo)
-		if Props.ShowReturnBack then
-			return Roact.createElement("Frame", {
-				Position = Props.Position,
-				Size = Props.Size,
-				BackgroundTransparency = 1
-			}, {
-				ReturnBack = Roact.createElement(StudioComponents.Button, {
-					Size = UDim2.new(0, 25, 1, 0),
-					Text = "<",
-					OnActivated = function()
-						RouterInfo.history:goBack()
-					end,
-				}),
-				Title = Roact.createElement(StudioComponents.Label, {
-					Size = UDim2.new(1, -25, 1, 0),
-					Position = UDim2.new(0, 25, 0, 0),
-					TextSize = 16,
-					Font = Enum.Font.SourceSansBold,
-					Text = Props.Title,
-				}),
-			})
-		else
-			return Roact.createElement(StudioComponents.Label, {
-				Position = Props.Position,
-				Size = Props.Size,
+	if Props.ShowReturnBack then
+		return Roact.createElement("Frame", {
+			Position = Props.Position,
+			Size = Props.Size,
+			BackgroundTransparency = 1,
+		}, {
+			ReturnBack = Roact.createElement(StudioComponents.Button, {
+				Size = UDim2.new(0, 25, 1, 0),
+				Text = "<",
+				OnActivated = function()
+					Props.OnReturn()
+				end,
+			}),
+			Title = Roact.createElement(StudioComponents.Label, {
+				Size = UDim2.new(1, -25, 1, 0),
+				Position = UDim2.new(0, 25, 0, 0),
+				TextSize = 16,
+				Font = Enum.Font.SourceSansBold,
 				Text = Props.Title,
-			})
-		end
-	end)
+			}),
+		})
+	else
+		return Roact.createElement(StudioComponents.Label, {
+			Position = Props.Position,
+			Size = Props.Size,
+			Text = Props.Title,
+			TextSize = 16,
+			Font = Enum.Font.SourceSansBold,
+		})
+	end
 end
--- src/Common/Topbar.story.lua
+++ src/Common/Topbar.story.lua
@@ -1,12 +1,29 @@
-return function(Parent)
-	local Message = Instance.new("TextLabel")
-	Message.Size = UDim2.new(1, 0, 1, 0)
-	Message.Text = "This component uses RoactRouter, which means I do not want"
-		.. " bother implementing the story for it."
+local Roact = require(script.Parent.Parent.Packages.Roact)
+local Topbar = require(script.Parent.Topbar)
 
-	Message.Parent = Parent
+return function(Parent)
+	local Handle = Roact.mount(
+		Roact.createFragment({
+			WithReturnBack = Roact.createElement(Topbar, {
+				Title = "Check output.",
+				Size = UDim2.new(0, 200, 0, 25),
+				Position = UDim2.new(0, 0, 0, 0),
+				ShowReturnBack = true,
+				OnReturn = function()
+					print("Returned!")
+				end,
+			}),
+			WithoutReturnBack = Roact.createElement(Topbar, {
+				Title = "Empty",
+				Size = UDim2.new(0, 200, 0, 25),
+				Position = UDim2.new(0, 0, 0, 25),
+				ShowReturnBack = false,
+			}),
+		}),
+		Parent
+	)
 
 	return function()
-		Message:Destroy()
+		Roact.unmount(Handle)
 	end
 end

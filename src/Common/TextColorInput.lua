83bc1b00f6cb5c3e3962a68942f1d7a430e54806
u-train <skassem05@outlook.com>
2022-01-19 16:09:24 -0500

    Added TextColorInput.lua documentation completely

--- src/Common/TextColorInput.lua
+++ src/Common/TextColorInput.lua
@@ -1,7 +1,10 @@
 --[=[
 	@class TextColorInput
-	A helper component which takes a tool and wraps it in a topbar and a input
-	for EditingIn. Additionally, places the tool into a scrollframe.
+	A component which allows a user to type in RGB values. There are three text
+	boxes for the respective channels. The value for each channel can range from
+	0 to 255, with 0.1 precision. In the future, this component will support
+	putting custom channels and custom types.
+	![Image of component rendered](/rendered/textColorInput.png)
 ]=]
 
 --[=[
@@ -11,6 +14,18 @@
 	.Position UDim2
 	.OnColorChanged (Color3) -> nil
 	.Color Color3
+	.Channels { Channel } -- Not yet implemented
+]=]
+
+--[=[
+	@within TextColorInput
+
+	Haven't been implemented yet.
+	@interface Channel
+	.Name string
+	.Min number
+	.Max number
+	.Precision number
 ]=]
 
 local Packages = script.Parent.Parent.Packages
@@ -22,13 +37,23 @@ local FormatColor = function(Input)
 	return ("%.1f"):format(Input * 255)
 end
 
+-- TODO: Put in as a default. Read docs for explaination.
 local Colors = {
 	"R",
 	"G",
 	"B",
 }
 
-local CreateColorChannelChanged = function(Props)
+--[=[
+	@within TextColorInput
+
+	Returns a function which given a channel will create the event handler
+	for when that channel changes.
+	@function CreateColorChannelChangedFactory
+	@param Props Props
+	@return (Channel: string) -> (Text: string) -> nil
+]=]
+local CreateColorChannelChangedFactory = function(Props)
 	return function(Channel)
 		return function(Text)
 			local NewNumber = tonumber(Text)
@@ -50,7 +75,7 @@ end
 
 return function(Props)
 	local Children = {}
-	local GivenColorChannelChanged = CreateColorChannelChanged(Props)
+	local GivenColorChannelChanged = CreateColorChannelChangedFactory(Props)
 
 	for Key, Color in next, Colors do
 		Children[Color] = Roact.createElement(ControlledInput, {
